62b1225852bb39756846e9ce3e7cb8e9d22078d6
hb6700 <hb6700@naver.com>
2023-09-04 17:38:07 +0900

    230904_수정

--- question.sql
+++ question.sql
@@ -700,3 +700,66 @@ WHERE address <> '서울시';
     
 -- employees. 자신의 매니저보다 먼저 고용된 사원들의 first_name, last_name, 고용일을 가져오시오.
 
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- 문제8.sql
+-- rownum + group by
+
+-- 1. tblInsa. 남자 급여(기본급+수당)을 (내림차순)순위대로 가져오시오. (이름, 부서, 직위, 급여, 순위 출력)
+SELECT * FROM tblinsa;
+SELECT name, buseo, jikwi, basicpay+sudang AS 급여, rownum
+FROM tblinsa
+WHERE substr(ssn, 8, 1) = '1'
+ORDER BY basicpay+sudang DESC;
+
+-- 2. tblInsa. 여자 급여(기본급+수당)을 (오름차순)순위대로 가져오시오. (이름, 부서, 직위, 급여, 순위 출력)
+SELECT * FROM tblinsa;
+SELECT name, buseo, jikwi, basicpay+sudang AS 급여, rownum
+FROM tblinsa
+WHERE substr(ssn, 8, 1) = '2'
+ORDER BY basicpay+sudang ASC;
+
+-- 3. tblInsa. 여자 인원수가 (가장 많은 부서 및 인원수) 가져오시오. ----
+SELECT * FROM tblinsa;
+SELECT buseo, count(*)
+FROM tblinsa
+WHERE substr(ssn, 8, 1) = '2'
+GROUP BY buseo;
+
+
+-- 4. tblInsa. 지역별 인원수 (내림차순)순위를 가져오시오.(city, 인원수)
+SELECT * FROM tblinsa;
+SELECT city, count(*) AS 인원수
+FROM tblinsa
+GROUP BY city
+ORDER BY 인원수 desc;
+
+-- 5. tblInsa. 부서별 인원수가 가장 많은 부서 및원수 출력.
+SELECT * FROM tblinsa;
+SELECT buseo, count(*) AS "부서별 인원수"
+FROM tblinsa
+GROUP BY buseo
+ORDER BY "부서별 인원수" DESC;
+
+-- 6. tblInsa. 남자 급여(기본급+수당)을 (내림차순) 3~5등까지 가져오시오. (이름, 부서, 직위, 급여, 순위 출력)
+SELECT * FROM tblinsa;
+SELECT name, buseo, jikwi, basicpay, rownum
+FROM tblinsa;
+
+
+
+-- 7. tblInsa. 입사일이 빠른 순서로 5순위까지만 가져오시오.
+
+
+-- 8. tblhousekeeping. 지출 내역(가격 * 수량) 중 가장 많은 금액을 지출한 내역 3가지를 가져오시오.
+
+
+-- 9. tblinsa. 평균 급여 2위인 부서에 속한 직원들을 가져오시오.
+
+
+-- 10. tbltodo. 등록 후 가장 빠르게 완료한 할일을 순서대로 5개 가져오시오.
+
+
+-- 11. tblinsa. 남자 직원 중에서 급여를 3번째로 많이 받는 직원과 9번째로 많이 받는 직원의 급여 차액은 얼마인가?
+
+
